0f05cb0268ead211443cd12f7612f2b4560ab1b3
TheTOXIN <thetoksin@gmail.com>
2022-05-29 00:46:01 +0300

    add load balance

--- customer/src/main/java/com/toxin/customer/CustomerServiceImpl.java
+++ customer/src/main/java/com/toxin/customer/CustomerServiceImpl.java
@@ -22,7 +22,7 @@ public class CustomerServiceImpl implements CustomerService {
         customerRepository.saveAndFlush(customer);
 
         FraudCheckResponse fraudResponse = restTemplate.getForObject(
-                "http://localhost:8081/api/v1/fraud/{customerId}",
+                "http://FRAUD/api/v1/fraud/{customerId}",
                 FraudCheckResponse.class,
                 customer.getId()
         );
--- fraud/src/main/java/com/toxin/fraud/FraudController.java
+++ fraud/src/main/java/com/toxin/fraud/FraudController.java
@@ -1,20 +1,25 @@
 package com.toxin.fraud;
 
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/v1/fraud")
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class FraudController {
 
     private final FraudService fraudService;
 
+    @Value("${eureka.instance.instance-id}")
+    private String instanceId;
+
     @GetMapping(path = "{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId) {
-        log.info("Fraud check request for customer {}", customerId);
+        log.info("Fraud check request for customer - {} in instance - {}", customerId, instanceId);
         return new FraudCheckResponse(
                 fraudService.isFraudCustomer(customerId)
         );
