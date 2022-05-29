2b45d26c17453113d6edeff83d329e84bc53dbd1
TheTOXIN <thetoksin@gmail.com>
2022-05-29 15:14:12 +0300

    add sleuth and zipkin and redis commander

--- api-gateway/src/main/java/com/toxin/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/toxin/apigateway/ApiGatewayApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 public class ApiGatewayApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
 }
--- notification/src/main/java/com/toxin/notification/NotificationService.java
+++ notification/src/main/java/com/toxin/notification/NotificationService.java
@@ -3,5 +3,6 @@ package com.toxin.notification;
 import com.toxin.clients.notification.NotificationRequest;
 
 public interface NotificationService {
+
     void send(NotificationRequest request);
 }
